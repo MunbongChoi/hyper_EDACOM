9d3a7f9cbba16b344ad51a6a625a11c5fbfbe4d7
parkh <a>
2021-08-22 04:44:08 +0900

    솨과

--- src/pages/dashboards/Project/Basic2.js
+++ src/pages/dashboards/Project/Basic2.js
@@ -1,25 +1,24 @@
 // @flow
 import React from 'react';
 import { Row, Col, Card, CardBody, Table } from 'reactstrap';
+import grade1 from "../../../assets/images/grade1.png";
+import grade2 from "../../../assets/images/grade2.png";
+import grade3 from "../../../assets/images/grade3.png";
+import grade4 from "../../../assets/images/grade4.png";
 //
 // import PageTitle from '../../components/PageTitle';
 
 const records = [
-    { rank: 1, grade: <i className="mdi mdi-food-apple"></i>, point: '10P', criteria: '200kW' },
-    { rank: 2, grade: <i className="mdi mdi-tree-outline"></i>, point: '5P', criteria: '210kW' },
-    { rank: 3, grade: <i className="mdi mdi-sprout-outline"></i>, point: '3P', criteria: '220kW' },
-    { rank: 4, grade: <i className="mdi mdi-seed-outline"></i>, point: '0P', criteria: '230kW' }
+    { rank: 1, grade: <img src={grade1} alt="" height="20" />, point: '10P', criteria: '200kW' },
+    { rank: 2, grade: <img src={grade2} alt="" height="20" />, point: '5P', criteria: '210kW' },
+    { rank: 3, grade: <img src={grade3} alt="" height="20" />, point: '3P', criteria: '220kW' },
+    { rank: 4, grade: <img src={grade4} alt="" height="20" />, point: '0P', criteria: '230kW' }
 ];
 
 const BasicTable = () => {
     return (
         <Card>
             <CardBody>
-                <h4 className="header-title">전력등급제 테이블</h4>
-                {/*<p className="text-muted font-14 mb-4">*/}
-                {/*    Just use <code>Table</code> element*/}
-                {/*</p>*/}
-
                 <Table className="mb-0">
                     <thead>
                         <tr>
@@ -27,223 +26,6 @@ const BasicTable = () => {
                             <th>등급</th>
                             <th>포인트</th>
                             <th>전력사용량</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const InverseTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Inverse Table</h4>
-                <p className="text-muted font-14 mb-4">
-                    You can also invert the colors—with light text on dark backgrounds—by specifying <code>dark</code>{' '}
-                    attribute
-                </p>
-
-                <Table className="mb-0" dark>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const StripedRowsTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Striped Rows</h4>
-                <p className="text-muted font-14 mb-4">
-                    Add <code>striped</code> attribute to table
-                </p>
-
-                <Table className="mb-0" striped>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const BorderedTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Bordered table</h4>
-                <p className="text-muted font-14 mb-4">
-                    Add <code>bordered</code> attribute for borders on all sides of the table and cells.
-                </p>
-
-                <Table className="mb-0" bordered>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const HoverableTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Hoverable Rows</h4>
-                <p className="text-muted font-14 mb-4">
-                    Add <code>hover</code> attribute to enable a hover state on table rows
-                </p>
-
-                <Table className="mb-0" hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const SmallTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Small table</h4>
-                <p className="text-muted font-14 mb-4">
-                    Add <code>size="sm"</code> attribute to make tables more compact by cutting cell padding in half
-                </p>
-
-                <Table className="mb-0" size="sm">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {records.map((record, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{record.rank}</th>
-                                    <td>{record.grade}</td>
-                                    <td>{record.point}</td>
-                                    <td>{record.criteria}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-            </CardBody>
-        </Card>
-    );
-};
-
-const ResponsiveTable = () => {
-    return (
-        <Card>
-            <CardBody>
-                <h4 className="header-title">Always responsive</h4>
-                <p className="text-muted font-14 mb-4">
-                    Across every breakpoint, use <code>responsive</code> attribute to create responsive tables
-                </p>
-
-                <Table className="mb-0" responsive>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
                         </tr>
                     </thead>
                     <tbody>
--- src/pages/dashboards/Project/Statistics.js
+++ src/pages/dashboards/Project/Statistics.js
@@ -2,6 +2,7 @@
 import React,{useEffect} from 'react';
 import { Card, CardBody, Row, Col } from 'reactstrap';
 import axios from "axios";
+import grade1 from "../../../assets/images/grade1.png";
 
 const Statistics = () => {
 useEffect(() => {
@@ -26,6 +27,19 @@ useEffect(() => {
                             <Row className="no-gutters">
                                 <Col sm={6} xl={3}>
                                     <Card className="shadow-none m-0">
+                                        <CardBody className="text-center">
+                                            <h3>
+                                                <span>
+                                                    <img src={grade1} alt="" height="50" />
+                                                </span>
+                                            </h3>
+                                            <p className="text-muted font-15 mb-0">나의 등급</p>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+
+                                <Col sm={6} xl={3}>
+                                    <Card className="card shadow-none m-0 border-left">
                                         <CardBody className="text-center">
                                             <i className="mdi mdi-alpha-p-box"></i>
                                             <h3>
@@ -56,18 +70,6 @@ useEffect(() => {
                                                 <span>31</span>
                                             </h3>
                                             <p className="text-muted font-15 mb-0">줄어든 탄소배출량 </p>
-                                        </CardBody>
-                                    </Card>
-                                </Col>
-
-                                <Col sm={6} xl={3}>
-                                    <Card className="card shadow-none m-0 border-left">
-                                        <CardBody className="text-center">
-                                            <i className="uil-trees"></i>
-                                            <h3>
-                                                <span>10그루</span> <i className="mdi mdi-arrow-up text-success"></i>
-                                            </h3>
-                                            <p className="text-muted font-15 mb-0">나의 등급</p>
                                         </CardBody>
                                     </Card>
                                 </Col>
